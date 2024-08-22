1cda2fcad69e6e6deb4352d2dda3d274e655b4fb
Lucas Panizio <lucas_panizio@hotmail.com>
2024-08-21 21:09:33 -0300

    :sparkles: feat: add input component

---
+++ src/components/molecules/Input/index.tsx
@@ -0,0 +1,25 @@
+import { InputHTMLAttributes, useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+import * as S from './styles';
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+export const Input = ({ name, ...props }: IInputProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = useMemo(() => {
+    return errors[name]?.message;
+  }, [errors, name]);
+
+  return (
+    <S.Container>
+      <S.Input {...register(name)} {...props} />
+      {error && <label>{error.toString()}</label>}
+    </S.Container>
+  );
+};
---
+++ src/components/molecules/Input/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  label {
+    color: #ef4444;
+  }
+`;
+
+export const Input = styled.input`
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+`;
