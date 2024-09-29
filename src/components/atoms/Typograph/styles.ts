7cb3609983c49a21275907139540a2d818b0f2eb
Lucas Panizio <lucas_panizio@hotmail.com>
2024-09-28 23:25:08 -0300

    :sparkles: feat: add typograph component

---
+++ src/components/atoms/Typograph/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Color } from '../../../types/color';
+
+export const typographySizes = {
+  h1: '2rem',
+  h2: '1.5rem',
+  h3: '1.25rem',
+  h4: '1.125rem',
+  h5: '1rem',
+  h6: '0.875rem',
+  p: '1rem',
+  span: '1rem',
+  small: '0.875rem',
+} as const;
+
+export interface TypographProps {
+  variant?: keyof typeof typographySizes;
+  color?: Color;
+}
+
+export const Typography = styled.span<TypographProps>`
+  font-size: ${({ variant = 'p' }) => typographySizes[variant]};
+  color: ${({ theme, color }) => color || theme.colors.text.light};
+  line-height: 1.5;
+`;
