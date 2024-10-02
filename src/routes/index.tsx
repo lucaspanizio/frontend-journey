dca07ed28edda2c7a81238f13d402e183a1699c9
Lucas Panizio <lucas_panizio@hotmail.com>
2024-10-02 17:33:39 -0300

    :recycle: refactor: updated routes and pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Routes as Router, Route } from 'react-router-dom';
-import { NotFound } from '../pages/NotFound';
-import { ContextAPI } from '../pages/ContextAPI';
-import { ReduxToolkit } from '../pages/ReduxToolkit';
-import { Zustand } from '../pages/Zustand';
 import { Layout } from '../components/templates/Layout';
+import { NotFound } from '../pages/NotFound';
+import { StateManagers } from '../pages/StateManagers';
 import { CompoundPattern } from '../pages/CompoundPattern';
 
 export const Routes: React.FC = () => {
@@ -13,9 +11,7 @@ export const Routes: React.FC = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<CompoundPattern />} />
         <Route path="/compound-pattern" element={<CompoundPattern />} />
-        <Route path="/context-api" element={<ContextAPI />} />
-        <Route path="/redux-toolkit" element={<ReduxToolkit />} />
-        <Route path="/zustand" element={<Zustand />} />
+        <Route path="/state-managers" element={<StateManagers />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Router>
